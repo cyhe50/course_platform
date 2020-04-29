e474a971edd018dde8fd606a5a2b62533289ac31
janice <>
2020-04-29 09:45:15 +0800

    modify courses management

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :courses
+
   root "courses#home"
+  scope :management do
+    resources :courses
+  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
